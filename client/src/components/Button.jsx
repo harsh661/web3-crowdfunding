6c45ddd0c63f211b19258b261a35b1f9c6fc850d
harsh661 <plasmaharsh@gmail.com>
2023-08-13 18:10:15 +0530

    add ui of top navbar

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label}) => {
   return (
-    <div className='bg-accent py-2 px-3 text-sm rounded-md'>
+    <div className='bg-accent/90 hover:bg-accent py-2 px-3 text-sm rounded-md font-bold transition-all duration-200 text-center cursor-pointer'>
         {label}
     </div>
   )
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='bg-dark-alt border-gray-border rounded-md w-full'>
+        <input type='text' className='bg-transparent text-gray-text p-2 text-sm w-full'/>
+    </div>
+  )
+}
+
+export default Search
