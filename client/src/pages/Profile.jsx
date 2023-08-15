ffd9bf2a4ca3b053cb77b61a3e67b1a8106273fe
harsh661 <plasmaharsh@gmail.com>
2023-08-15 11:15:06 +0530

    feat: add profile page to fetch user campaigns

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react"
+import { useContractContext } from "../context"
+import CampaignsGrid from "../components/campaigns/CampaignsGrid"
+import Heading from "../components/Heading.jsx"
 
 const Profile = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const { address, contract, getCampaigns } = useContractContext()
+
+  const fetchCampaigns = async () => {
+    setLoading(true)
+    const campaigns = await getCampaigns()
+    setData(campaigns.filter((item) => item.owner === address))
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (contract) {
+      fetchCampaigns()
+    }
+  }, [address, contract])
+
   return (
-    <div>Profile</div>
+    <div className="p-5 max-w-7xl mx-auto">
+        <Heading title="My Campaigns" subtitle="Discover all campaings you started"/>
+        <CampaignsGrid data={data} loading={loading} />
+    </div>
   )
 }
 
