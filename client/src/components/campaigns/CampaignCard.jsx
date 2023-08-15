30d1f381e782c79b2e8118ffcbca9e3975f03c4a
harsh661 <plasmaharsh@gmail.com>
2023-08-15 15:16:55 +0530

    feat: add progress bar to campaign card

--- client/src/components/campaigns/CampaignCard.jsx
+++ client/src/components/campaigns/CampaignCard.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { daysLeft } from "../../utils"
+import { PiUserCircle } from "react-icons/pi"
+import ProgressBar from "./ProgressBar"
 
 const CampaignCard = ({
   owner,
@@ -13,24 +15,24 @@ const CampaignCard = ({
 }) => {
   const remainingDays = daysLeft(deadline)
   return (
-    <div
-      onClick={handleClick}
-      className="border border-gray-border bg-dark-alt/70 text-warm-white rounded-xl px-2"
-    >
-      <div className="flex flex-col gap-1 p-3 overflow-hidden">
-        <span className="text-xs text-white">Started by</span>
+    <div className="border border-gray-border bg-dark-alt/70 text-warm-white rounded-xl px-2">
+      <div className="flex items-center gap-1 p-3 overflow-hidden">
+        {/* <span className="text-xs text-white">Started by</span> */}
+        <PiUserCircle size={20} className="text-accent" />
         <span className="text-xs text-gray-text truncate">{owner}</span>
       </div>
       <img
+        onClick={handleClick}
         src={image}
         alt={title}
-        className="w-full aspect-video object-cover rounded-xl border border-gray-border"
+        className="w-full aspect-video object-cover rounded-xl border border-gray-border cursor-pointer"
       />
       <div className="p-3">
         <h2 className="text-white line-clamp-1 font-medium">{title}</h2>
-        <p className="text-xs text-gray-text line-clamp-2 mt-1">
-          {description}
-        </p>
+      </div>
+
+      <div className="p-3">
+        <ProgressBar goal={target} raisedAmount={amountCollected}/>
       </div>
       <div className="p-3 flex items-center justify-between">
         <div className="flex flex-col gap-0">
