7466b225a07f80e271c01826056d4b5b183aa5e0
harsh661 <plasmaharsh@gmail.com>
2023-08-15 10:15:40 +0530

    feat: show skeleton loader while fetching data

--- client/src/components/campaigns/CampaignsGrid.jsx
+++ client/src/components/campaigns/CampaignsGrid.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import CampaignCard from "./CampaignCard"
+import Skeleton from "../Skeleton"
 
 const CampaignsGrid = ({ data, loading }) => {
-  if (loading) return <div>Loading...</div>
+  if (loading) return <Skeleton />
 
   return (
-    <div className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+    <div className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {data.map((item) => (
         <CampaignCard key={item.id} {...item} />
       ))}
